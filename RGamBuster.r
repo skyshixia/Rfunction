43cd3197a0efc69837bae20e8ada68cbf65f30d8
Shia <xiaoxias@uci.edu>
2015-11-11 15:46:40 -0800

    Original upload

---
+++ RGamBuster.r
@@ -0,0 +1,39 @@
+## To source this file into R session, type:
+# source('https://raw.githubusercontent.com/skyshixia/Rfunction/master/RGamBuster.r')
+
+## input
+# percentile: percentile, e.g. 0.05, 0.99 etc
+# percentileValue: the prior value at qth percentile
+# momeValue: prior mode/mean
+# mu=TRUE if the input value is mean, default is FALSE (input is mode)
+
+RGamBuster <- function(percentile, percentileValue, momeValue, mu=FALSE){
+	qth = percentile
+	qthValue = percentileValue
+	stats = momeValue
+	
+	if(mu == TRUE){
+			b2a <- function(stats, b){stats*b}
+			print("Given mean,")
+		}else{
+			b2a <- function(stats, b){stats*b+1}
+			print("Given mode,")
+	}
+
+	b = 1:1000/100
+	a = b2a(stats, b)
+	li = qgamma(qth, a, b)
+
+	intb = which((diff(li >= qthValue, lag=1)) != 0)/100
+
+	decb = intb + b/1000
+	a = b2a(stats, decb)
+	li2 = qgamma(qth, a, decb)
+
+	realb = round(intb + which((diff(li2 >= qthValue, lag=1)) != 0)/100000, 3)
+	reala = round(b2a(stats, realb), 3)
+
+	answer = list("a is", reala, "b is", realb)
+	return(answer)
+}
+
